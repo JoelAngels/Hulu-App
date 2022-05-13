0c1ebd50f10fd1a93b1d4eb7f42fa95b0904bc25
Joel Angel <joelbaraka4325@gmail.com>
2022-05-13 11:11:03 +0300

    styled the titles but havent yuet put any plugins

--- components/Nav.js
+++ components/Nav.js
@@ -5,10 +5,15 @@ function Nav() {
     <nav>
       {/* loop through the request and give me the title of each one */}
 
-      <div>
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll">
         {/* Any item mapped through anr array, item or object, reacts needs a key so that it knows how to rerender the list*/}
         {Object.entries(requests).map(([key, { title, url }]) => (
-          <h2 key={key}>{title}</h2>
+          <h2
+            key={key}
+            className="last:p-r-24cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
+          >
+            {title}
+          </h2>
         ))}
       </div>
       {/* 1.04 */}
