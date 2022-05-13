b0b6eb25a28b974e77ad0ed398aff177a5277ead
Joel Angel <joelbaraka4325@gmail.com>
2022-05-13 14:19:39 +0300

    project done

--- components/Results.js
+++ components/Results.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
 
 function Results({ results }) {
   return (
-    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {/* Get a request then map through  every single one and every single result that comes back lets rended them as a thumbnail */}
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
-    </div>
+    </FlipMove>
   );
 }
 
