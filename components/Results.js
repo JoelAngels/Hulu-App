823ed7b819322eaa303476d3a625db7298fd4d66
Joel Angel <joelbaraka4325@gmail.com>
2022-05-13 13:41:18 +0300

    added custom theme screen in tailwind config and changed grom grid to flex depending with screen size

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import Thumbnail from "./Thumbnail";
 
 function Results({ results }) {
   return (
-    <div>
+    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {/* Get a request then map through  every single one and every single result that comes back lets rended them as a thumbnail */}
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
